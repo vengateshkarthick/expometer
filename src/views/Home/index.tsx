f32fe75142288e5ab65ef1378010b26a2b3d156d
VengateshGV <53400321+vengateshkarthick@users.noreply.github.com>
2023-11-26 16:21:28 +0000

    Created ui for log card

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import EarningsHeader from "../EarningsHeader";
-import "./home.scss";
 import { useLogData } from "../../generics/useLogData/useLogData";
+import loader from '../../assets/icons8-spinner.gif';
+import "./home.scss";
 
 function Home() {
-  const {expenseData, walletData, isExpenseDataLoading, isWalletDataLoading} = useLogData();
+  const { expenseData, walletData, isExpenseDataLoading, isWalletDataLoading } = useLogData();
   
   return (
     <div className="home-container">
       <EarningsHeader />
       <div className="accordion-card">
-        
+        {(isExpenseDataLoading || isWalletDataLoading) ?
+          <div className="loader"><img src={loader} /></div>
+          : 
+          <div>
+            
+          </div>
+        }
       </div>
     </div>
   );
